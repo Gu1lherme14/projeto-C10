1e55198d833f9bc580ab82d8bfadef36be7511c4
Gu1lherme14 <98350725+Gu1lherme14@users.noreply.github.com>
2022-02-04 16:29:05 -0300

    Add files via upload

---
+++ pro-c10-sa1-template-main/sketch.js
@@ -0,0 +1,17 @@
+var peso = [35,38,40,45]
+
+
+function setup() 
+{
+  createCanvas(400,400);
+  var soma = peso[0] + peso[1] + peso[2] + peso[3]
+  var media = soma/ peso.length
+  console.log(media)
+}
+
+function draw() 
+{
+background(51);
+
+}
+
